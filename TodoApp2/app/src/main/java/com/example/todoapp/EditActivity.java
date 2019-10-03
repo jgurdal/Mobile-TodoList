0e9e7f361a460f8eb5d13da85ebc88f47912f20f
Jonathan <jgurdal@mail.sfsu.edu>
2019-10-02 21:34:43 -0700

    Added Todo Code
    Updated completed todo application

---
+++ TodoApp2/app/src/main/java/com/example/todoapp/EditActivity.java
@@ -0,0 +1,44 @@
+package com.example.todoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditActivity extends AppCompatActivity {
+
+    EditText editItem;
+    Button btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        editItem = findViewById(R.id.editItem);
+        btnSave  = findViewById(R.id.btnSave);
+
+        getSupportActionBar().setTitle("Edit Item");
+
+        editItem.setText(getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT));
+
+        //When the user is done editing, they click the save button
+        btnSave.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //create an intent which will contain the results
+                Intent intent = new Intent();
+                //pass the data (results of editing)
+                intent.putExtra(MainActivity.KEY_ITEM_TEXT,editItem.getText().toString());
+                intent.putExtra(MainActivity.KEY_ITEM_POSITION, getIntent().getExtras().getInt(MainActivity.KEY_ITEM_POSITION));
+                //set the result of the intent
+                setResult(RESULT_OK, intent);
+                //finish the activity, close the screen and go back
+                finish();
+            }
+        });
+    }
+}
